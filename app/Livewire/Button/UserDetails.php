ef49f6af5d4b6fa949ed1886b64c49f06bcdf091
hassnae <hassnae@gmail.com>
2023-08-29 10:12:15 +0100

    Properties: Public,Private and protected

--- app/Livewire/Button/UserDetails.php
+++ app/Livewire/Button/UserDetails.php
@@ -6,8 +6,19 @@ use Livewire\Component;
 
 class UserDetails extends Component
 {
+    public $firstName;
+    public $lastName;
+    public function mount(){
+        $this->firstName = "John";
+        $this ->lastName = "Doe";
+        $this->fill([
+            'firstname' => 'Jenna',
+            'lastName' => 'Stones'
+        ]);
+    }
+
     public function render()
     {
-        return view('livewire.button.user-details');
+        return view('livewire.user-details');
     }
 }
